074689a8b1b01f775a4a1ee55c481a1b0caf25f0
George Stocker <gjstocker@gmail.com>
2014-04-17 07:21:47 -0400

    removing copyrighted text
    Removing copyrighted text until permission is obtained.

--- src/js/jstrek.instructions.js
+++ src/js/jstrek.instructions.js
@@ -1,10 +1,7 @@
 var instructions = window.jstrek.instructions || {};
 
 var SECTION_1_INSTRUCTIONS = "Welcome Aboard Captain!\n\nWill you require a briefing (Y/N)? ";
-var SECTION_2_INSTRUCTIONS = "Good morning, Captain. AS you know, you have been given command of the\n Research/BattleCruiser U.S.S. Lexington, standard fleed designation\n RCB-92.  The Lexington is staffed with its full complement of 387\n";
-SECTION_2_INSTRUCTIONS += "enlisted crewmen and 43 officers and is fully battle ready.\n\n";
-SECTION_2_INSTRUCTIONS += "If you will allow me, I have prepared several briefings, including an\n intelligence report on the current state of affairs, an introduction\n to Lexington Class IX starships, and the commands at your disposal as\n captain.\n\n";
 
-
+var section_1_choices = ['Y','N'];
 
 window.jstrek.instructions = instructions;
