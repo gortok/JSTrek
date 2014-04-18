852418972af989a99b15fb6fb53b4fcdf972df95
George Stocker <gjstocker@gmail.com>
2014-04-18 17:16:19 -0400

    generating nearstarfield prototype
    wewt.

--- src/js/jsTrek.main.js
+++ src/js/jsTrek.main.js
@@ -1,69 +1,66 @@
 var jsTrek.main = window.jstTrek.main || {};
 
-var star2 = { 
+var star = { 
 	chance: 08,
 	types: ['active'],
 	display: '*'
 };
 
-var base2 = {
+var starBase = {
 	chance: 01,
 	types: ['research','supply','friendly','enemy'],
 	display: '@'
 	
 };
 
-var planet2 = {
+var planet = {
 	chance: 01,
 	types: ['O','M','K','Z'],
 	display: '$'
 };
 
-var choices = [star2,base2,planet2];
-
-var nearStarField = [[]];
-
-var populateNearStarField = function() {
-	var chosen = [];
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
-			for (var k = 0; nearStarField[j,i] == undefined && k < choices.length; k++) {
-				if (getRandomArbitrary(1,100) <= choices[k].chance && !containsObject(choices[k], chosen)) {
-					nearStarField[j,i] = choices[k];
-					if (choices[k] != star2) {
-						chosen.push(choices[k]);
-					}
-					console.log(choices[k].display);
-					break;
-				}
-			}	
-		}	
-	}
-	for (var i = 0; i < 10; i++) {
-		var line = '';
-		for (var j = 0; j < 10; j++) {
-			if (nearStarField[j,i] == undefined) {
-				line += ".";
-			}
-			else {
-				line += nearStarField[j,i].display;
-			}
-		}
-		console.log(line);
-	}
-	console.dir(nearStarField);
-	
+var empty = {
+	display: '.'
 };
-var containsObject = function(obj, list) {
-	var i;
-	for (i = 0; i < list.length; i++) {
-		if (list[i] === obj) {
-			return true;
+
+var choices = [star,starBase,planet];
+
+var starField = [];
+var createStarField = function() {
+	for (var k = 1; k <= 64; k++) {
+		var choice = determineObject(choices);
+		if (choice !== undefined) {
+			starField[k] = choice;
+		}
+		else {
+			starField[k] = empty.display;
 		}
 	}
-	return false;
-}
+};
+var determineObject = function(arr) {
+	console.dir(arr);
+	var chosenObject = undefined;
+	for (var i = 0; i < arr.length; i++) {
+		if (chosenObject !== undefined) { break; }
+		if (getRandomArbitrary(1,64) <= arr[i].chance) {
+			chosenObject = arr[i].display;
+		}
+	}
+	return chosenObject;
+};
 
 var getRandomArbitrary = function(min,max) {
 	return Math.floor(Math.random() * (max-min + 1)) + min;
 };
+
+var paintStarField = function(starField) {
+	var field = "";
+	console.dir(starField);
+	for(var i = 1; i < starField.length; i++) {
+		field += starField[i];
+		if (i % 8 == 0) {
+			field += "\n";
+		}
+	}
+	return field;
+}
